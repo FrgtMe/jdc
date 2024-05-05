8dcec500e8171c2c496017c32d66332a9feab46e
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-05-05 09:39:44 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
+wget https://www.python.org/ftp/python/3.10.5/Python-3.10.5.tar.xz
+tar -xf Python-3.10.5.tar.xz
+cd Python-3.10.5
+./configure
+make
+sudo make install
+curl https://bootstrap.pypa.io/get-pip.py > pip.py
+python3 pip.py
 pip install notebook
 echo -e "e\ne" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
