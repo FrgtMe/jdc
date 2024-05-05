0827d971acb99feec6fc622acd5b709641a13764
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-05-05 11:40:32 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 wget https://www.python.org/ftp/python/3.10.5/Python-3.10.5.tgz
 tar -xvzf Python-3.10.5.tgz
-cd Python-3.10.5.tgz
+cd Python-3.10.5
 ./configure --prefix=$HOME/python3.10.5
 make
 sudo make install
