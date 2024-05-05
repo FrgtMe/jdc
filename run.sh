94b8b0e24b166317c61d8d583edb02c3da9a27c8
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-05-05 10:44:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 wget https://www.python.org/ftp/python/3.10.5/Python-3.10.5.tgz
 tar -xvzf Python-3.10.5.tgz
 cd Python-3.10.5.tgz
-./configure
+./configure --prefix=$HOME/python3.10.5
 make
 sudo make install
 curl https://bootstrap.pypa.io/get-pip.py > pip.py
-python3 pip.py
+python3.10.5 pip.py
 pip install notebook
 echo -e "e\ne" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
