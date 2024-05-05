155ad32a058e4a6756c6c949f7c6d72bf641083a
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-05-05 11:36:50 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,6 @@ cd Python-3.10.5.tgz
 make
 sudo make install
 curl https://bootstrap.pypa.io/get-pip.py > pip.py
-python-3.10.5 pip.py
+python pip.py
 pip install notebook
 echo -e "e\ne" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
