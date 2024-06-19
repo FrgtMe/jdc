a36f3b4d0b7d9930940944796a6ef5f6e314afbf
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-06-19 13:53:01 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 pip3 install selenium notebook 
-echo -e "e\ne" | jupyter notebook password && jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
+echo -e "e\ne" | jupyter notebook password 
+jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
