40ac57d2b5b2b6ea6f412f02069fe3443c867680
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-06-21 08:23:10 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
-pip3 install --upgrade setuptools
-pip3 install ez_setup
-pip3 install selenium notebook 
+cat pip.txt > ~/.config/pip/pip.conf
+pip3 install selenium notebook --break-system-packages
 echo -e "e\ne" | jupyter notebook password 
 jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
