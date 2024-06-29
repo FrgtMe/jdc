50a5e9b1ad7dfd05ea21e9519d6f01ed5217ca88
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-06-29 14:01:36 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 pip install flask
 apt install wget -y
-python app.py while true; do wget https://proper-lesley-babapirohosting-a5275c04.koyeb.app/; done
+python app.py & while true; do wget https://proper-lesley-babapirohosting-a5275c04.koyeb.app/; done
