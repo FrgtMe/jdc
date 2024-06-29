30f357f0c6590940d1d0584e39b31cedfe86712c
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-06-29 13:32:21 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 apt install wget -y
-python app.py &
-while true; do https://proper-lesley-babapirohosting-a5275c04.koyeb.app/; done
+python app.py & while true; do wget https://proper-lesley-babapirohosting-a5275c04.koyeb.app/; done
